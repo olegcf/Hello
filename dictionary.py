76f35be6526a94a7ba9c7e62f72f10d94d2f9b20
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2021-10-02 21:22:02 +0300

    more on list

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,11 @@
-countries = [ 'Israel', 'Czechia', 'Ukraine' ]
+countries = [ 'Israel', 'Czechia', 'Ukraine', 'Moldova' ]
 
 print(countries)
 print(countries[1])
-print(countries[1:2])
+print(len(countries))
 print('Slice:',countries[1:2])
-print('Slice:',countries[1,2])
+
+
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print(a[2:4])
